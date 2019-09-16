82123366ee6ad56e4a02d9f6a8dfe7267ae718be
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-16 18:33:21 -0400

    runable

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,12 +2,21 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def directors_totals
-  directors_database.reduce({}) do |memo, d|
-    memo[d[:name]] = gross_for_director(d)
-    memo
-  end
+  # Write this out as a nested series of whiles
 end
 
-def gross_for_director(d)
-  d[:movies].reduce(0){ |dir_total, m| dir_total += m[:worldwide_gross]; dir_total }
+def worldwide_gross_for_directors(nds)
+  # Write a method that loops through the collection of directors and builds a
+  # total of all their movies' worldwide grosses. This method should call
+  # gross_for_director to do the "totalling" work.
+end
+
+def gross_for_director(nds, director_name)
+  # Write a method that, given an NDS and a directors name returns the totals
+  # of that directors movies based on summing an AoH where each Hash has a key
+  # :worldwide_gross
+  #
+  # Hint:
+  # * Find the right director
+  # * Once found, iterate the director's movies and total up their gross
 end
