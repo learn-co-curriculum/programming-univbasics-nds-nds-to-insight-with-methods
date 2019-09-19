40c6924b38650dfcaf33a0953afa670b45c7bab9
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-19 17:28:26 -0400

    rm unused method

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,12 +4,6 @@ require 'directors_database'
 def directors_totals(nds)
 end
 
-def worldwide_gross_for_directors(nds)
-  # Write a method that loops through the collection of directors and builds a
-  # total of all their movies' worldwide grosses. This method should call
-  # gross_for_director to do the "totalling" work.
-end
-
 def gross_for_director(director_name)
   # Write a method that, given an NDS and a directors name returns the totals
   # of that directors movies based on summing an AoH where each Hash has a key
